0a9ba0ee6f8fe2a99e4bdee3be1f6617ad58df6f
BrianFigueroa <bryan09figueroa@gmail.com>
2024-07-02 14:56:46 -0300

    add access

--- app/(client)/sign-in/[[...sign-in]]/page.tsx
+++ app/(client)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,16 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn path="/sign-in" forceRedirectUrl={"/wizard"} />;
+    return (
+        <>
+        <div className="absolute top-3 flex flex-col opacity-50">
+            access
+            <span>email:  test1@gmail.com</span>
+                <span>password: test@112233</span>
+
+        </div>
+    <SignIn path="/sign-in" forceRedirectUrl={"/wizard"} />
+        </>
+    )
+    
 }
